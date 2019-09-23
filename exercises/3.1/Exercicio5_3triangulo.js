15a3a66ede610678d9d07e0bf97948a44b1c7497
Henrique Eyer <henriqueeyer@outlook.com>
2019-09-23 17:48:38 -0300

    Exercicio5_3triangulo.js arrumando o programa.

--- exercises/3.1/Exercicio5_3triangulo.js
+++ exercises/3.1/Exercicio5_3triangulo.js
@@ -1,5 +1,21 @@
-let ang1=82;
-let ang2=19;
-let ang3=79;
+let ang1;
+let ang2;
+let ang3;
 
-((ang1+ang2+ang3)==180)?console.log("Eh um traingulo"):console.log("N eh um triangulo");
+let controle;
+let angulovalido=false;
+
+do{
+
+    ang1=94;
+    ang2=5;
+    ang3=81;
+
+
+((ang1>0 && ang2>0 && ang3>0) )?angulovalido=true:console.log("Os angulos precisam serem validos");
+
+}while(angulovalido!=true)
+
+((ang1+ang2+ang3)==180)?controle=true:controle=false;
+
+controle?console.log(controle):console.log("N eh um triangulo");
