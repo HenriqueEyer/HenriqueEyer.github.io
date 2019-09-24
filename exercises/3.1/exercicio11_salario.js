142c94812a00e3df792ba81434ef005003250da5
Henrique Eyer <henriqueeyer@outlook.com>
2019-09-24 09:21:09 -0300

    Exercicio 11 pronto

--- exercises/3.1/exercicio11_salario.js
+++ exercises/3.1/exercicio11_salario.js
@@ -1,4 +1,4 @@
-let bruto=2500.00;
+let bruto=4000.00;
 let inss;
 let ir;
 if(bruto<=1556.94){
@@ -10,15 +10,17 @@ if(bruto<=1556.94){
 }else{
     inss=570.88;
 }
-
-if(bruto<=1903.94){
+ brutoinss=bruto-inss;
+if(brutoinss<=1903.94){
     ir=0;
-}else if(bruto<=2826.65){
-    ir=bruto*0.075-142.80;
-}else if(bruto<=3751.05){
-    ir=bruto*0.15-354.80;
-}else if(bruto<=4664.68){
-    ir=bruto*0.5-636.13;
+}else if(brutoinss<=2826.65){
+    ir=brutoinss*0.075-142.80;
+}else if(brutoinss<=3751.05){
+    ir=brutoinss*0.15-354.80;
+}else if(brutoinss<=4664.68){
+    ir=brutoinss*0.225-636.13;
 }else{
-    inss=570.88;
+    inss=brutoinss*0.275-869.36;
 }
+
+console.log(brutoinss-ir);
