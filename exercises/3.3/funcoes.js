a01e1863d9004e5b1247eee168c241a400b0c179
Henrique Eyer <henriqueeyer@outlook.com>
2019-09-25 17:17:47 -0300

    Exercicios 1 ao 3 prontos

---
+++ exercises/3.3/funcoes.js
@@ -0,0 +1,74 @@
+//exercicio 1
+//Definição que eu já vi de diferença entre eles: -For é muito bom para percorrer um array pq em um array ele possuem um final entao voce delimita um maximo para rodar, o while pode ser usado para manter uma validação de dados, rodar sem parar até a pessoa digitar o dado valido. 
+
+function exer1(){
+
+    let palavra=document.getElementById("entrada").value;
+    palavra=palavra.toLowerCase();
+
+    let letras=palavra.split("");
+    let i;
+    let controle=true;
+    let metadepalavra;
+    if(letras.length%2==0){
+        metadepalavra=(letras.length/2);
+    }else{
+        metadepalavra=((letras.length+1)/2)
+    }
+    for(i=0;i<metadepalavra;i++){
+        if(letras[i]!=letras[(letras.length-1)-i]){
+            controle=false;
+        }
+    }
+    
+    let resultado=document.getElementById("lblresultado");
+    resultado.innerHTML=controle;
+    
+}
+
+
+function exer2(){
+
+    let entrada=document.getElementById("entrada2").value;
+    
+    let split=entrada.split("");
+    
+    let maior=split[0];
+
+    let indice=0;
+
+    console.log(split);
+
+    for(i=0;i<split.length;i++){
+       if(split[i]>maior){
+        maior=split[i];
+        indice=i;
+       }
+    }
+    
+    let resultado=document.getElementById("lblresultado2");
+    resultado.innerHTML="Maior: "+maior+", Indice:"+indice;
+    
+}
+
+function exer3(){
+
+    let entrada=document.getElementById("entrada3").value;
+    
+    let split=entrada.split("");
+    
+    let menor=split[0];
+
+    let indice=0;
+
+    for(i=0;i<split.length;i++){
+       if(split[i]<menor){
+        menor=split[i];
+        indice=i;
+       }
+    }
+    
+    let resultado=document.getElementById("lblresultado3");
+    resultado.innerHTML="Menor: "+menor+", Indice:"+indice;
+    
+}
