aa15ff7b18ea04ce0cbef9edba6db6f00621b5ba
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-30 19:22:42 -0300

    Agora sim finalizado

--- exercises/8.3/exercise6.js
+++ exercises/8.3/exercise6.js
@@ -5,14 +5,7 @@ const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
 const chiron = ["Chiron", "Bugatti", 2016]
 
 // escreva toObject abaixo
-const toObject = ([a,b,c]) => {
-    const obj = {
-        name: a,
-        brand:b,
-        year:c
-    }
-    return obj
- }
+const toObject = ([name, brand, year]) => ({ name, brand, year})
 assert.deepEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
 assert.deepEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
 assert.deepEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })
