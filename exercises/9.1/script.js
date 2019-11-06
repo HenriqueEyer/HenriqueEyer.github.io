539c22f91c350d5e9d8e67741f398523dedeedb1
Henrique Eyer <henriqueeyer@outlook.com>
2019-11-06 19:15:42 -0300

    removendo comentarios

--- exercises/9.1/script.js
+++ exercises/9.1/script.js
@@ -1,29 +1,22 @@
 
 window.onload = () => {
-  
-const returnByid = name => document.getElementById(name)
-const returnByClassName = name => document.getElementsByClassName(name)
-const setNewKey=({id,value})=>localStorage.setItem(id,value)
-const setStyle=(changeStyle,valueStyle)=>document.body.style[changeStyle]=valueStyle
-
-for(let value of Object.keys(localStorage)){
-    setStyle(value,localStorage.getItem(value))
-}
-
-function allValues(){
-    let values=returnByClassName('values');
-    
-    for(let val of values){
-       if(val.value){
-           setNewKey(val)
-           setStyle(val.id,val.value)
-       } 
+    const returnByid = name => document.getElementById(name)
+    const returnByClassName = name => document.getElementsByClassName(name)
+    const setNewKey = ({ id, value }) => localStorage.setItem(id, value)
+    const setStyle = (changeStyle, valueStyle) => document.body.style[changeStyle] = valueStyle
+    for (let value of Object.keys(localStorage)) {
+        setStyle(value, localStorage.getItem(value))
     }
-
-}
-
-returnByid('btnSave').addEventListener('click',event=>{
-    allValues();
-})
-
+    function allValues() {
+        let values = returnByClassName('values');
+        for (let val of values) {
+            if (val.value) {
+                setNewKey(val)
+                setStyle(val.id, val.value)
+            }
+        }
+    }
+    returnByid('btnSave').addEventListener('click', event => {
+        allValues();
+    })
 }
