97572dfa8a32be51449f5930bb694f0a867191e3
Henrique Eyer <henriqueeyer@outlook.com>
2019-11-14 18:58:55 -0300

    Terminando os exercicios

---
+++ exercises/10.2/my-pokedex/src/DetailsPokemon.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './DetailsPokemon.css'
+
+class DetailsPokemon extends React.Component {
+    render() {
+        const {id, name, type, image, moreInfo, averageWeight } = this.props.pokemon;
+
+        const itemtype = (item, size) => {
+            return <li>{item} {size}</li>
+        }
+
+        const link = (item, info) => {
+            return <li><a href={item}>  Mais info sobre {info} </a></li>
+        }
+
+        const imgPoke = imgPokemon => {
+            return <img src={imgPokemon} />
+        }
+        return (
+            <div className="Details">
+                <span className="id">{id}</span>
+                <span>{name}</span>
+                <div className="sub">
+                <ul>
+                    {itemtype(type)}
+                    {itemtype(averageWeight.value, averageWeight.measurementUnit)}
+                    {link(moreInfo,name)}
+                </ul>
+                {imgPoke(image)}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DetailsPokemon;
